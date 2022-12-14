c8c337b79497dc1f22a9e9e5ae5781e5f63c6217
Tessa van Abkoude <tessa@moeilijkedingen.nl>
2022-12-14 14:42:39 +0100

    Updates

---
+++ precache-manifest.ee21de252f206db5a70501b3ce67b152.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "e65ce3bdb2b4829a71ad",
+    "url": "/Quetta/static/css/main.e9f144ea.chunk.css"
+  },
+  {
+    "revision": "e65ce3bdb2b4829a71ad",
+    "url": "/Quetta/static/js/main.e65ce3bd.chunk.js"
+  },
+  {
+    "revision": "5a63e80967d755f43234",
+    "url": "/Quetta/static/js/1.5a63e809.chunk.js"
+  },
+  {
+    "revision": "d00f19bf43c0aa110659",
+    "url": "/Quetta/static/js/2.d00f19bf.chunk.js"
+  },
+  {
+    "revision": "85397e61ad56a4a2fe7b",
+    "url": "/Quetta/static/js/runtime~main.85397e61.js"
+  },
+  {
+    "revision": "e839a8bf81ebbd61c03cc6be233498e0",
+    "url": "/Quetta/static/media/instagram.e839a8bf.svg"
+  },
+  {
+    "revision": "21ea74e97c29902eef9d8da027ca2b3a",
+    "url": "/Quetta/static/media/YouTube.21ea74e9.svg"
+  },
+  {
+    "revision": "e1361c1c66e5ad2bbba3e9d523500bc6",
+    "url": "/Quetta/static/media/TikTok.e1361c1c.svg"
+  },
+  {
+    "revision": "c71bc0c88111069aafbd635f23bb9153",
+    "url": "/Quetta/static/media/whatsapp.c71bc0c8.svg"
+  },
+  {
+    "revision": "d07c27d5ff1341632d469a4bb371bd52",
+    "url": "/Quetta/static/media/Email.d07c27d5.svg"
+  },
+  {
+    "revision": "ead7928048c6ec7a8d2390d199e219ef",
+    "url": "/Quetta/static/media/Netflix.ead79280.svg"
+  },
+  {
+    "revision": "fb8f1752d67f7c7384c604ec242a948b",
+    "url": "/Quetta/static/media/Twitter.fb8f1752.svg"
+  },
+  {
+    "revision": "c48815026d84fdfa359243984aa43305",
+    "url": "/Quetta/static/media/Steam.c4881502.svg"
+  },
+  {
+    "revision": "d762dcadb86731aee1003157fe17543b",
+    "url": "/Quetta/static/media/snapchat.d762dcad.svg"
+  },
+  {
+    "revision": "4b7bddf78939ae2a468d7839e38f1218",
+    "url": "/Quetta/static/media/spotify.4b7bddf7.svg"
+  },
+  {
+    "revision": "5bdb380bf03ee62563488f613e413b95",
+    "url": "/Quetta/static/media/Facebook.5bdb380b.svg"
+  },
+  {
+    "revision": "3ef03c49293502a2360648763a031d3d",
+    "url": "/Quetta/static/media/Pinterest.3ef03c49.svg"
+  },
+  {
+    "revision": "5fac5025e09a8d6817dc4280b45a4047",
+    "url": "/Quetta/static/media/Skype.5fac5025.svg"
+  },
+  {
+    "revision": "06c51f0136893d8fd79192bb5b33e697",
+    "url": "/Quetta/static/media/googlechrome.06c51f01.svg"
+  },
+  {
+    "revision": "20d3b9280a57c8f6c88b57c76e8be8c7",
+    "url": "/Quetta/static/media/Teams.20d3b928.svg"
+  },
+  {
+    "revision": "b516a0324e01dc706e45d9842ae10289",
+    "url": "/Quetta/static/media/News.b516a032.svg"
+  },
+  {
+    "revision": "3dfb6de271924e1b66fa52a9aac40eb1",
+    "url": "/Quetta/static/media/shopping.3dfb6de2.svg"
+  },
+  {
+    "revision": "59aaf1c9a112952c7e3c9e717289e595",
+    "url": "/Quetta/index.html"
+  }
+];
